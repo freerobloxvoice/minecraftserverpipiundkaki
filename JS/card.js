beeb2365926a75db69215d9a3fcd76c7fa6a4d3f
freerobloxvoice <freerobloxvocicechat1@gmail.com>
2026-02-05 17:22:27 +0200

    Add files via upload

---
+++ JS/card.js
@@ -0,0 +1,16 @@
+let card = JSON.parse(localStorage.getItem("card"))||[]
+
+function addtocard(id,name,price,img){
+    let item = card.find(p => p.id === id)
+    if(item){
+        item.qty+=1
+    }
+    else{
+        card.push(
+            {
+                id,name,price,img,qty:1
+            }
+        )
+    }
+    savecard()
+}
